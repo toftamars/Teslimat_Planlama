fa0a65266c8fd4566b31c0f47aeead5fb01f11d8
toftamars <GitHub E-posta Adresiniz>
2025-08-16 23:03:53 +0300

    Manuel Düzenle (Yönetici) butonu tamamen kaldırıldı

--- teslimat_planlama/models/teslimat_ana_sayfa.py
+++ teslimat_planlama/models/teslimat_ana_sayfa.py
@@ -1,5 +1,4 @@
 from odoo import models, fields, api
-from odoo.exceptions import AccessError
 from datetime import datetime, timedelta
 
 
@@ -242,19 +241,4 @@ class TeslimatAnaSayfa(models.Model):
             }
         }
     
-    def action_manuel_duzenle(self):
-        """Sadece yönetici tarafından manuel düzenleme"""
-        self.ensure_one()
-        
-        # Yönetici kontrolü
-        if not self.env.user.has_group('stock.group_stock_manager'):
-            raise AccessError("Bu işlem için yönetici yetkisi gereklidir!")
-        
-        return {
-            'type': 'ir.actions.act_window',
-            'res_model': 'teslimat.ana.sayfa.tarih',
-            'view_mode': 'tree,form',
-            'domain': [('ana_sayfa_id', '=', self.id)],
-            'context': {'default_ana_sayfa_id': self.id},
-            'name': 'Tarih Kapasite Düzenleme (Yönetici)'
-        }
+
