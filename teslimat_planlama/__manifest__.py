be2a9c71bb9846cc83aca617f1fef2ca84f9cdd9
toftamars <GitHub E-posta Adresiniz>
2025-09-06 01:23:42 +0300

    Gereksiz dosyalar silindi, basit object metod kullanıldı

--- teslimat_planlama/__manifest__.py
+++ teslimat_planlama/__manifest__.py
@@ -31,8 +31,6 @@
                'views/teslimat_ana_sayfa_views.xml',
                'views/teslimat_belgesi_wizard_views.xml',
                'views/teslimat_konum_wizard_views.xml',
-               'views/teslimat_hizli_olustur_wizard_views.xml',
-               'views/teslimat_basit_olustur_views.xml',
                'views/teslimat_planlama_views.xml',
                'views/menu_views.xml',
            ],
