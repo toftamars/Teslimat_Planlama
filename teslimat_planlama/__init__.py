f77891c96ab86315954f1827615d7ad24dd9308a
toftamars <GitHub E-posta Adresiniz>
2025-11-02 01:12:08 +0300

    fix: Post-init hook fonksiyonu düzeltildi - doğru Environment kullanımı

--- teslimat_planlama/__init__.py
+++ teslimat_planlama/__init__.py
@@ -4,15 +4,24 @@ from . import wizards
 
 def post_init_hook(cr, registry):
     """Post-install/upgrade hook: Eski model referanslarını temizle."""
-    env = registry['base'].env(cr=cr)
+    from odoo import api, SUPERUSER_ID
+    env = api.Environment(cr, SUPERUSER_ID, {})
     # Eski teslimat.planlama.akilli model referanslarını temizle
-    env['ir.model.data'].search([
-        ('module', '=', 'teslimat_planlama'),
-        ('model', '=', 'teslimat.planlama.akilli')
-    ]).unlink()
-    # Eski model tanımını temizle
-    env['ir.model'].search([
-        ('model', '=', 'teslimat.planlama.akilli')
-    ]).unlink()
-    cr.commit()
+    try:
+        old_data = env['ir.model.data'].search([
+            ('module', '=', 'teslimat_planlama'),
+            ('model', '=', 'teslimat.planlama.akilli')
+        ])
+        if old_data:
+            old_data.unlink()
+        # Eski model tanımını temizle
+        old_model = env['ir.model'].search([
+            ('model', '=', 'teslimat.planlama.akilli')
+        ])
+        if old_model:
+            old_model.unlink()
+        cr.commit()
+    except Exception:
+        # Model yoksa hata verme, sadece log
+        cr.rollback()
 
